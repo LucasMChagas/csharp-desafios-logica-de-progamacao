8ce9137c6c063833df168ae85fea09ac21eb8688
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2024-04-08 21:06:25 -0300

    Desafio Extremamente Básico

---
+++ ExtremamenteBasico/Program.cs
@@ -0,0 +1,10 @@
+﻿//Leia 2 valores inteiros e armazene-os nas variáveis A e B.
+//Efetue a soma de A e B atribuindo o seu resultado na variável X.
+//Imprima X conforme exemplo apresentado abaixo.
+
+var a = int.Parse(Console.ReadLine());
+var b = int.Parse(Console.ReadLine());
+
+var x = a + b;
+
+Console.WriteLine($"X = {x}");

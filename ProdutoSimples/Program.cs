af105875859530b4426c768721972056aaf72481
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2024-04-10 11:38:58 -0300

    Desafio Produto Simples

---
+++ ProdutoSimples/Program.cs
@@ -0,0 +1,11 @@
+﻿// Leia dois valores inteiros. 
+// A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
+// A seguir mostre a variável PROD com mensagem correspondente.   
+
+var a = int.Parse(Console.ReadLine());
+var b = int.Parse(Console.ReadLine());
+
+var PROD = a * b;
+
+Console.WriteLine($"PROD = {PROD}");
+

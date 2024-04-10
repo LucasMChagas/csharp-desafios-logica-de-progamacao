4d4b21dbba17d68f446dce2aee5cf343542670de
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2024-04-10 11:26:53 -0300

    Desafio Soma Simples

---
+++ SomaSimples/Program.cs
@@ -0,0 +1,11 @@
+﻿// Leia dois valores inteiros, no caso para variáveis A e B. 
+// A seguir, calcule a soma entre elas e atribua à variável SOMA. 
+// A seguir escrever o valor desta variável.
+
+var a = int.Parse(Console.ReadLine());
+var b = int.Parse(Console.ReadLine());
+
+var soma = a + b;
+
+Console.WriteLine($"SOMA = {soma}");
+

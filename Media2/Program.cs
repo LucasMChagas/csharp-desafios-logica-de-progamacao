61f37b63fe8694b8e6adbbde0045aea8d0994ed1
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2024-04-18 10:26:18 -0300

    Desafio Média 2

---
+++ Media2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno.
+//A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5.
+//Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+
+var a = Convert.ToDouble(Console.ReadLine());
+var b = Convert.ToDouble(Console.ReadLine());
+var c = Convert.ToDouble(Console.ReadLine());
+
+var media = ((a * 2) + (b * 3) + (c * 5)) / 10;
+
+Console.WriteLine($"MEDIA = {media.ToString("F1")}");

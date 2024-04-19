41dbe980187570b5a96e813a223c8f1cc107130b
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2024-04-19 11:51:32 -0300

    Desafio Diferença

---
+++ Diferença/Program.cs
@@ -0,0 +1,11 @@
+﻿//Leia quatro valores inteiros A, B, C e D.
+//A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+var a = Convert.ToInt32(Console.ReadLine());
+var b = Convert.ToInt32(Console.ReadLine());
+var c = Convert.ToInt32(Console.ReadLine());
+var d = Convert.ToInt32(Console.ReadLine());
+
+var diferenca = (a * b - c * d);
+
+Console.WriteLine($"DIFERENCA = {diferenca}");

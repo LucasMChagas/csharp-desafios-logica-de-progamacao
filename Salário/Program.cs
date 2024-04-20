3e269f55ee8215f95e69f4d844c79be25d84be6e
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2024-04-20 11:01:49 -0300

    Desafio Salário

---
+++ Salário/Program.cs
@@ -0,0 +1,18 @@
+﻿//Escreva um programa que leia o número de um funcionário,
+//seu número de horas trabalhadas,
+//o valor que recebe por hora
+//e calcula o salário desse funcionário.
+//A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+
+using System.Globalization;
+
+var numeroFuncionario = Convert.ToInt32(Console.ReadLine());
+var horasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+var salarioHora = Convert.ToDouble(Console.ReadLine());
+
+var salario = horasTrabalhadas * salarioHora;
+
+
+
+Console.WriteLine($"NUMBER = {numeroFuncionario}");
+Console.WriteLine($"SALARY = U$ {salario.ToString("F2", new CultureInfo("en-US"))}");
